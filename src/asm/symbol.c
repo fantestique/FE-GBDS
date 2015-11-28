b11594cd4ba48b899b5a51464182ae2f883c4190
FANTESTIQUE <fantestique@users.noreply.github.com>
2015-11-28 17:30:07 +0800

    updates for temps

--- src/asm/symbol.c
+++ src/asm/symbol.c
@@ -431,10 +431,11 @@ sym_AddNewMacroArg(char *s)
 		i += 1;
 
 	if (i < MAXMACROARGS) {
-		if (s)
+		if (s){
 			char* temps= (char*)(malloc(strlen(s)+1));
 			if(temps!=NULL) strcpy(temps,s);
 			newmacroargs[i] = temps;// replace the strdup(s)
+		}
 		else
 			newmacroargs[i] = NULL;
 	} else
